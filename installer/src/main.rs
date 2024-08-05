54ec3a9533dc6d1f76a969b31b65223bf9b58ba4
Angus Peart <angusp@gmail.com>
2024-08-04 17:35:07 -0700

    installer: handle installing webview2 when needed

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -7,25 +7,23 @@ use crate::webview2::Webview2;
 fn main() {
     // Handle bundled external program
     let binary = Binary::load();
-    println!("Binary data length: {}", binary.data.len());
     println!("External program: {}", binary.name);
 
     // Handle bundled WebView2 runtime
     let webview2 = Webview2::load();
-    if webview2.bundled {
-        println!("Webview2 bundled: true");
-        println!("Webview2 data length: {}", webview2.data.unwrap().len());
-    } else {
-        println!("Webview2 bundled: false");
-    }
+    println!("Webview2 bundled: {}", webview2.bundled);
 
     // Check if WebView2 runtime is installed
     let webview2_installed = webview2.installed;
     println!("Webview2 installed: {}", webview2_installed);
+    if !webview2_installed {
+        println!("Installing webview2 runtime...");
+        webview2
+            .install(false)
+            .expect("Failed to install webview2 runtime");
+    }
 
     // TODO:
-    // - Check for webview2 runtime
-    // - Run webview2 installer if needed
     // - Extract and copy the bundled installer
     // - Set the windows registry keys
     // - Handle uninstall with command line flag
--- installer/src/webview2.rs
+++ installer/src/webview2.rs
@@ -1,29 +1,31 @@
+use anyhow::{anyhow, Result};
 use std::env;
 use std::fs;
-
-#[cfg(target_os = "windows")]
-use winreg::RegKey;
+use std::process::Command as Process;
 
 pub struct Webview2 {
     pub bundled: bool,
     pub data: Option<Vec<u8>>,
     pub installed: bool,
+
+    name: String,
 }
 
 impl Webview2 {
     pub fn load() -> Self {
         let bundled = env!("WEBVIEW2_BUNDLED") == "true";
+        let name = env!("WEBVIEW2_BUNDLED_NAME").to_string();
 
         let mut data = None;
         if bundled {
-            let name = env!("WEBVIEW2_BUNDLED_NAME").to_string();
             data = Some(Self::load_data(&name));
         }
 
         Self {
             bundled,
-            data: data,
+            data,
             installed: Self::installed(),
+            name,
         }
     }
 
@@ -33,29 +35,68 @@ impl Webview2 {
         println!("Webview2 exe path: {}", binary_path);
         fs::read(&binary_path).expect("Failed to read external program binary")
     }
+}
 
-    #[cfg(target_os = "windows")]
+#[cfg(target_os = "windows")]
+impl Webview2 {
     fn installed() -> bool {
-        let system_wide_64bit = RegKey::predef(HKEY_LOCAL_MACHINE).open_subkey("SOFTWARE\\WOW6432Node\\Microsoft\\EdgeUpdate\\Clients\\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}").is_err();
-        let system_wide_32bit = RegKey::predef(HKEY_LOCAL_MACHINE)
-            .open_subkey(
-                "SOFTWARE\\Microsoft\\EdgeUpdate\\Clients\\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}",
-            )
-            .is_err();
-        let user_32bit_64bit = RegKey::predef(HKEY_CURRENT_USER)
-            .open_subkey(
-                "SOFTWARE\\Microsoft\\EdgeUpdate\\Clients\\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}",
-            )
-            .is_err();
-        if system_wide_64bit && system_wide_32bit && user_32bit_64bit {
+        use webview2_com::{Microsoft::Web::WebView2::Win32::*, *};
+        use windows::core::{PCWSTR, PWSTR};
+        let mut versioninfo = PWSTR::null();
+        let result = unsafe {
+            GetAvailableCoreWebView2BrowserVersionString(PCWSTR::null(), &mut versioninfo)
+        };
+
+        if result.is_err() || versioninfo == PWSTR::null() {
+            return false;
+        }
+
+        let version = take_pwstr(versioninfo);
+        if version.len() > 0 {
+            println!("WebView2 version: {}", version);
             return true;
         }
 
         false
     }
 
-    #[cfg(not(target_os = "windows"))]
+    pub fn install(&self, quiet: bool) -> Result<()> {
+        let args = if quiet {
+            vec!["/silent", "/install"]
+        } else {
+            vec!["/install"]
+        };
+
+        // Copy the installer to a temp location
+        let temp_dir = env::temp_dir();
+        let installer_path = temp_dir.join(self.name.clone());
+        fs::write(&installer_path, self.data.as_ref().unwrap()).expect("Failed to write installer");
+
+        // Run the installer
+        println!("Running installer: '{:?}', args={:?}", installer_path, args);
+        let mut cmd = Process::new(installer_path).args(&args).spawn()?;
+        let result: i32 = cmd
+            .wait()?
+            .code()
+            .ok_or_else(|| anyhow!("Unable to get installer exit code."))?;
+
+        match result {
+            0 => Ok(()),           // success
+            -2147219416 => Ok(()), // already installed
+            _ => Err(anyhow!("Installer failed with exit code: {}", result)),
+        }
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+impl Webview2 {
     fn installed() -> bool {
         true
+    }
+
+    pub fn install(&self, _quiet: bool) -> Result<()> {
+        Err(anyhow!(
+            "WebView2 installation is not supported on this platform."
+        ))
     }
 }
