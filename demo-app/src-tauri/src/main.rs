8070cb38c864ed37888d400794c11266028b2f3e
Angus Peart <angusp@gmail.com>
2024-08-08 01:09:15 -0700

    fixup! demo: add uninstall cli flag hook

--- demo-app/src-tauri/src/main.rs
+++ demo-app/src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 fn main() {
     // Handle uninstall on Windows
     #[cfg(target_os = "windows")]
-    tauri_windows_installer::handle_uninstall(&"com.gussy.demo-app"); // TODO: Get this at build time
+    tauri_windows_installer::handle_uninstall(&"Demo App", &"com.gussy.demo-app"); // TODO: Get this at build time
 
     demo_app_lib::run()
 }
