556f894af833ceac56d641b0c70a2afa65802624
Angus Peart <angusp@gmail.com>
2025-08-11 16:25:31 -0700

    .

--- installer/build.rs
+++
@@ -1,9 +0,0 @@
-use embed_manifest::{embed_manifest, new_manifest};
-
-fn main() {
-    // Embed the app.manifest file
-    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        embed_manifest(new_manifest("app.manifest")).expect("unable to embed manifest file");
-        println!("cargo:rerun-if-changed=app.manifest");
-    }
-}
